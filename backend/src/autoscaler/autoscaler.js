062a8825b8286249cc90284535828d580f8ed3e0
varnikarathi <varnikarathi24@gmail.com>
2026-02-04 13:56:09 +0530

    Autoscaler logic

---
+++ backend/src/autoscaler/autoscaler.js
@@ -0,0 +1,15 @@
+const {generateMetrics}=require("../services/metrics.service");
+const {evaluateScaling}=require("../services/scaling.service");
+function startAutoScaler(){
+    setInterval(()=>{
+        const metrics=generateMetrics();
+        const scalingDecision=evaluateScaling(metrics.cpu);
+        console.log("==============AUTOSCALER================");
+        console.log("CPU:",metrics.cpu);
+        console.log("Memory:",metrics.memory);
+        console.log("Requests/Sec:",metrics.requestsPerSecond);
+        console.log("Scaling Action:",scalingDecision.action);
+        console.log("Current Instances:",scalingDecision.currentInstances)
+    },5000);
+}
+MediaSourceHandle.exports={startAutoscaler};
