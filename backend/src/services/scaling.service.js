f14ec71a05ed729c8315ee4b2e1c7be5ec77f4d2
varnikarathi <varnikarathi24@gmail.com>
2026-02-10 00:31:24 +0530

    new scaling

--- backend/src/services/scaling.service.js
+++ backend/src/services/scaling.service.js
@@ -1,15 +1,31 @@
 let currentInstances=1;
 const MAX_INSTANCES=5;
 const MIN_INSTANCES=1;
-function evaluateScaling(cpuUsage){
+
+let lastScale=0;
+const COOLDOWN=20000;                                      // refresh every 20 seconds
+
+
+function evaluateScaling(cpuUsage, confidence){
+    const now= Date.now();
+    if(now-lastScale<COOLDOWN){
+        return{action:"COOLDOWN",currentInstances};
+    }
+
     let action="NO_ACTION";
-        if(cpuUsage >70 && currentInstances< MAX_INSTANCES){
+
+    if(cpuUsage >70 && currentInstances< MAX_INSTANCES && confidence>0.6){
         currentInstances++;
-        action="SCALE UP";}
-    else if(cpuUsage<30 && currentInstances>MIN_INSTANCES){
+        action="SCALE UP";
+        lastScale=now;
+    }
+
+    else if(cpuUsage<30 && currentInstances>MIN_INSTANCES && confidence>0.6){
         currentInstances--;
         action="SCALE_DOWN";
+        lastScale=now;
     }
+    
     return {action,currentInstances};
 }
 module.exports={evaluateScaling};
